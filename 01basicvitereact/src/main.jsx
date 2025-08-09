1e0e39082247c00eb190f71ea4c1b203712360bb
Sunny Kumar <143649318+sunnybhardwaj2027@users.noreply.github.com>
2025-08-09 23:15:38 +0530

    created my own react library and jsx

--- 01basicvitereact/src/main.jsx
+++ 01basicvitereact/src/main.jsx
@@ -1,9 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
+
+function MyApp(){
+  return (
+    <h1>chai aur react</h1>
+  )
+}
+
+/*
+
+const ReactElement = {
+    type : 'a',
+    props : {
+        href : 'https://google.com',
+        target : '_blank',
+    },
+    children : 'click me to visit google'
+}
+
+*/
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const variable = 'sunny you are doing good';
+
+const ReactElement = React.createElement(
+  'a',
+  {href : 'https://google.com', target : '_blank'},
+  'click me to visit google',
+  variable
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {ReactElement}
+    <MyApp />
+    {anotherElement}
     <App />
   </StrictMode>,
 )
